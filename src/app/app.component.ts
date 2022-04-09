40b48a6f44b75f9aad0906b516634492e1f56b09
David Ullmer <davidullmer@outlook.de>
2022-04-09 15:52:34 +0200

    Add webcam to homepage
    Co-authored-by: Tina <tina-hoeflich@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import {Subject, Observable} from 'rxjs';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'cloud-projekt';
+
+  click() {
+    this.title += "Hallo";
+    console.log("TEst button");
+  }
+ // toggle webcam on/off
+ public showWebcam = true;
+ public deviceId: string = "";
+ public videoOptions: MediaTrackConstraints = {
+   // width: {ideal: 1024},
+   // height: {ideal: 576}
+ };
+ public errors: WebcamInitError[] = [];
+
+ // latest snapshot
+ public webcamImage: WebcamImage = {} as WebcamImage;
+
+ // webcam snapshot trigger
+ private trigger: Subject<void> = new Subject<void>();
+
+ public ngOnInit(): void {
+   WebcamUtil.getAvailableVideoInputs()
+     .then((mediaDevices: MediaDeviceInfo[]) => {});
+ }
+
+ public triggerSnapshot(): void {
+   this.trigger.next();
+   console.log("hallo ich hab ein Bild gemacht");
+ }
+
+ public toggleWebcam(): void {
+   this.showWebcam = !this.showWebcam;
+ }
+
+ public handleInitError(error: WebcamInitError): void {
+   this.errors.push(error);
+ }
+
+
+ public handleImage(webcamImage: WebcamImage): void {
+   console.info('received webcam image', webcamImage);
+   this.webcamImage = webcamImage;
+ }
+
+ public uploadToAPI(): void {
+
+ }
+
+
+ public get triggerObservable(): Observable<void> {
+   return this.trigger.asObservable();
+ }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {WebcamModule} from 'ngx-webcam';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
